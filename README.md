05e40dd343ec80b040378b77df6669529db9110b
andys8 <andys8@users.noreply.github.com>
2018-10-15 21:15:59 +0200

    Upgrade to elm 0.19
    Updates the api and the example. A change in the elm api for `style` [1] leads to a change to return `Attribute msg` instead of `(String, String)`. The example and the documentation is adapted.
    
    [1] <https://github.com/elm/compiler/blob/master/upgrade-docs/0.19.md#functions-changed>

--- README.md
+++ README.md
@@ -1,48 +1,47 @@
 # Elm Transit Style
 
-    elm package install etaque/elm-transit-style
+    elm install etaque/elm-transit-style
 
-HTML animations for [elm-transit](http://package.elm-lang.org/packages/etaque/elm-transit/latest)'s `Transition`, to be used on elm-html `style` attribute. At the moment we have those, though it's easy to add more:
+HTML animations for [elm-transit](https://package.elm-lang.org/packages/etaque/elm-transit/latest)'s `Transition`, to be used on elm-html [`Attribute`](https://package.elm-lang.org/packages/elm/html/latest/Html#Attribute). At the moment we have those, though it's easy to add more:
 
-* `fade`
-* `slide`
-* `zoom`
-* `fadeSlide`
-* `fadeZoom`
-
-An ideal companion to [elm-transit-router](http://package.elm-lang.org/packages/etaque/elm-transit-router/latest).
+- `fade`
+- `slide`
+- `zoom`
+- `fadeSlide`
+- `fadeZoom`
 
 ## How it works
 
 A transition is composed of two phases: `Exit` then `Enter`. A style for a phase can be constructed with this signature:
 
 ```elm
-Float -> Style
+Float -> List (Attribute msg)
 ```
 
-where the `Float` is the clock of transition phase, varying linear from 0 to 1. A `Style` is just an alias to `List (String, String)`.
+where the `Float` is the clock of transition phase, varying linear from 0 to 1.
 
 A complete transition animation is constructed by composing exit and enter animations on a transition:
 
 ```elm
-compose : (Float -> Style) -> (Float -> Style) -> Transition -> Style
+compose : (Float -> List (Attribute msg)) -> (Float -> List (Attribute msg)) -> Transition -> List (Attribute msg)
 ```
 
 It can then be used on a transition. Example for fade and left slide animation, with a 50px offset:
 
 ```elm
   div
-    [ style (TransitStyle.fadeSlide 100 model.transition) ]
+    ( TransitStyle.fadeSlide 100 model.transition )
     [ text "Some content" ]
 ```
 
 Example for fade and zoom animation with a 5% offset:
+
 ```elm
   div
-    [ style (TransitStyle.fadeZoom 0.05 model.transition) ]
+    ( TransitStyle.fadeZoom 0.05 model.transition )
     [ text "Some content" ]
 ```
 
 ## Credits
 
-* Easings are backed by [Easing](http://package.elm-lang.org/packages/Dandandan/Easing/latest) package.
+- Easings are backed by [Easing](https://package.elm-lang.org/packages/elm-community/easing-functions/latest) package.
--- example/Main.elm
+++ example/Main.elm
@@ -1,77 +1,110 @@
-module Main exposing (..)
+module Main exposing (Model, Msg(..), Page(..), init, main, pageBg, subscriptions, update, view)
 
-import Html.App as Html
+import Browser
 import Html exposing (..)
-import Html.Events exposing (..)
 import Html.Attributes exposing (..)
-import Transit
+import Html.Events exposing (..)
+import Transit exposing (Step(..))
 import TransitStyle
 
 
 type alias Model =
-  Transit.WithTransition { page : Page }
+    Transit.WithTransition { page : Page }
 
 
-type Page =
-  Page1 | Page2
+type Page
+    = Page1
+    | Page2
 
 
 type Msg
-  = Click Page
-  | SetPage Page
-  | TransitMsg (Transit.Msg Msg)
+    = Click Page
+    | SetPage Page
+    | TransitMsg (Transit.Msg Msg)
 
 
-init : (Model, Cmd Msg)
-init =
-  ({ page = Page1, transition = Transit.empty }, Cmd.none)
+init : a -> ( Model, Cmd Msg )
+init _ =
+    ( { page = Page1, transition = Transit.empty }, Cmd.none )
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
+    case msg of
+        Click page ->
+            Transit.start TransitMsg (SetPage page) ( 100, 200 ) model
 
-    Click page ->
-      Transit.start TransitMsg (SetPage page) (100, 200) model
+        SetPage page ->
+            ( { model | page = page }, Cmd.none )
 
-    SetPage page ->
-      ({ model | page = page }, Cmd.none)
-
-    TransitMsg transitMsg ->
-      Transit.tick TransitMsg transitMsg model
+        TransitMsg transitMsg ->
+            Transit.tick TransitMsg transitMsg model
 
 
 view : Model -> Html Msg
 view model =
-  div
-    [ style [ ("margin", "20px 100px"), ("overflow-x", "hidden") ] ]
-    [ nav []
-      [ a [ onClick (Click Page1) ] [ text "To page 1" ]
-      , a [ onClick (Click Page2) ] [ text "To page 2" ]
-      ]
-    , div
-        [ style <| (TransitStyle.fadeSlide 200 model.transition) ++ [("background", pageBg model.page)]]
-        [ p [] [ text (toString model.page) ]
-        , p [] [ text (toString (Transit.getStep model.transition)) ]
-        , p [] [ text (toString (Transit.getValue model.transition)) ]
+    div
+        [ style "margin" "20px 100px", style "overflow-x" "hidden" ]
+        [ nav []
+            [ a [ onClick (Click Page1) ] [ text "To page 1" ]
+            , a [ onClick (Click Page2) ] [ text "To page 2" ]
+            ]
+        , div
+            (style "background" (pageBg model.page) :: TransitStyle.fadeSlide 200 model.transition)
+            [ p [] [ text (pageToString model.page) ]
+            , p [] [ text (stepToString (Transit.getStep model.transition)) ]
+            , p [] [ text (String.fromFloat (Transit.getValue model.transition)) ]
+            ]
         ]
-    ]
+
 
 pageBg : Page -> String
 pageBg page =
-  case page of
-    Page1 -> "red"
-    Page2 -> "green"
+    case page of
+        Page1 ->
+            "red"
+
+        Page2 ->
+            "green"
+
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Transit.subscriptions TransitMsg model
+    Transit.subscriptions TransitMsg model
 
 
+main : Program {} Model Msg
 main =
-  Html.program
-    { init = init
-    , update = update
-    , view = view
-    , subscriptions = subscriptions
-    }
+    Browser.element
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
+
+
+-- helper
+
+
+stepToString : Step -> String
+stepToString step =
+    case step of
+        Exit ->
+            "Exit"
+
+        Enter ->
+            "Enter"
+
+        Done ->
+            "Done"
+
+
+pageToString : Page -> String
+pageToString page =
+    case page of
+        Page1 ->
+            "Page1"
+
+        Page2 ->
+            "Page2"
